34568d21b58dc128905f56aa98ba0e47efe33898
esm2017siva <esm2017siva@gmail.com>
2017-02-08 12:30:01 +0530

    new file babelrc added

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('Hello World');
+import fs from "fs";
+import utilities from "util";
+
+const bar = fs.readFileSync("bar.json", "utf-8");
+const bar_to_json = JSON.parse(bar);
+console.log(utilities.inspect(bar_to_json));
