a99a03b0063400ad2d55e7aa186a7064ab45af8c
chsivarajendraprasad <chsivarajendraprasad@gmail.com>
2017-02-08 16:59:23 +0530

    updating index.js

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ import utilities from "util";
 const bar = fs.readFileSync("bar.json", "utf-8");
 const bar_to_json = JSON.parse(bar);
 console.log(utilities.inspect(bar_to_json));
+console.log('Hello')
