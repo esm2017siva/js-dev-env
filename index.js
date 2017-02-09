ae35cd9dfede7edaa646b000c76dfee291691f2a
esm2017siva <esm2017siva@gmail.com>
2017-02-09 12:28:11 +0530

    change1 in b1

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ import utilities from "util";
 const bar = fs.readFileSync("bar.json", "utf-8");
 const bar_to_json = JSON.parse(bar);
 console.log(utilities.inspect(bar_to_json));
-console.log('Hello')
+console.log('Hello World') // this change happened in branch b1
+
